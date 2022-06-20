ef187f2d5798a918dd1f5953aa55c27e95e48045
Eduardo Barreto <eduardopontobarreto@gmail.com>
2022-06-19 21:13:52 -0300

    add: led class

--- LastDance/config/led.h
+++ LastDance/config/led.h
@@ -6,3 +6,51 @@
 #define ledStatus1 27
 #define ledStatus2 28
 #define ledStatus3 29
+
+class led
+{
+private:
+    byte pin;
+    unsigned long lastBlink;
+
+    void init()
+    {
+        pinMode(pin, OUTPUT);
+    }
+
+public:
+    bool state;
+
+    led(byte _pin)
+    {
+        this->pin = _pin;
+        this->init();
+    }
+
+    void on()
+    {
+        state = 1;
+        digitalWrite(pin, state);
+    }
+
+    void off()
+    {
+        state = 0;
+        digitalWrite(pin, state);
+    }
+
+    void toggle()
+    {
+        state = !state;
+        digitalWrite(pin, state);
+    }
+
+    void blink(int time = 100)
+    {
+        if (millis() < lastBlink + time)
+            return;
+
+        this->toggle();
+        lastBlink = millis();
+    }
+};
