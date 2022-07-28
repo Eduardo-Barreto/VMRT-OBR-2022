763688ba6d7a8e1a3bc0c096a25a04c02560e6fe
Eduardo Barreto <eduardopontobarreto@gmail.com>
2022-07-28 01:21:33 -0300

    fix: catcher methods

--- LastDance/routines/catcher.h
+++ LastDance/routines/catcher.h
@@ -1,16 +1,33 @@
-void raiseCatcher()
-{
-    catcher.write(90);
-}
 
 void lowerCatcher()
 {
+    if (catcher.pos == 0)
+        return;
     catcher.write(0);
+}
+
+void raiseCatcher()
+{
+    if (catcher.pos == 90)
+        return;
+    catcher.write(90);
 }
 
 void layCatcher()
 {
+    if (catcher.pos == 160)
+        return;
     catcher.write(160);
+}
+
+void toggleCatcher()
+{
+    if (catcher.pos == 90)
+        layCatcher();
+    else if (catcher.pos == 0)
+        raiseCatcher();
+    else if (catcher.pos == 160)
+        lowerCatcher();
 }
 
 void catchBall()
@@ -23,10 +40,14 @@ void catchBall()
 
 void openBlocker()
 {
+    if (catcherBlocker.pos == 110)
+        return;
     catcherBlocker.write(110);
 }
 
 void closeBlocker()
 {
+    if (catcherBlocker.pos == 85)
+        return;
     catcherBlocker.write(85);
 }
